fe72190804d9c2cf73e29d0ca5ae32fee5b5a871
Johnneylee.Rollins@gmail.com <Johnneylee.Rollins@gmail.com>
2013-01-09 08:45:07 -0500

    Use cat and heredocs
    Signed-off-by: Johnneylee.Rollins@gmail.com <Johnneylee.Rollins@gmail.com>

--- install_in_omz.sh
+++ install_in_omz.sh
@@ -4,7 +4,7 @@ echo "Creating symlink from here to your oh-my-zsh themes path..."
 
 ln -f powerline.zsh-theme ~/.oh-my-zsh/themes/powerline.zsh-theme
 
-echo "
+cat <<-EOF
 Done ! (the symlink is ~/.oh-my-zsh/themes/powerline.zsh-theme)
 
 -----------------------------------------------------------------
@@ -35,3 +35,4 @@ Requirements:
 Vim Powerline patched font: See Powerline for vim for more info.
 Z shell (zsh): See oh-my-zsh for more info.
 Make sure terminal is using 256-colors mode with export TERM=\"xterm-256color\"".
+EOF
